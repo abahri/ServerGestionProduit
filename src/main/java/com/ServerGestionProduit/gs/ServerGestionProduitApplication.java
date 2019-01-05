27cd3244cd87c16194ae34d8ad1f353a8e6edc82
BAHRI <abdelaziz.bahri@cgi.com>
2019-01-05 21:22:13 +0100

    First commit start project

---
+++ src/main/java/com/ServerGestionProduit/gs/ServerGestionProduitApplication.java
@@ -0,0 +1,14 @@
+package com.ServerGestionProduit.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionProduitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ServerGestionProduit/gs/ServerGestionProduitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ServerGestionProduit.gs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerGestionProduitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
